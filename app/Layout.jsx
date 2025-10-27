0c5ffd0ac96a7dc0e350f382cd959d94ed7c7c3c
souhail <souhaibadaoui@gmail.com>
2025-10-27 23:13:48 +0100

    master1

--- app/Layout.jsx
+++ app/Layout.jsx
@@ -1,22 +1,22 @@
 
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// import Welcome from '../components/Welcome';
-// import Home from './index';
+import Welcome from '../components/Welcome';
+import Home from './index';
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator
-//         initialRouteName="Welcome"
-//         screenOptions={{ headerShown: false }}
-//       >
-//         <Stack.Screen name="Welcome" component={Welcome} />
-//         <Stack.Screen name="Home" component={Home} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
